a0b381634879d1f8ae2445e0733e20fda11a3965
hajun05 <hajun0325175@gmail.com>
2024-12-17 12:46:22 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 188 ms, Memory: 19816 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+
+namespace CsWorkspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = Convert.ToInt32(sr.ReadLine());
+            (int x, int y)[] coor = new (int x, int y)[N];
+            for (int i = 0; i < N; i++)
+            {
+                string[] input = sr.ReadLine().Split();
+                coor[i] = (int.Parse(input[0]), int.Parse(input[1]));
+            }
+
+            Array.Sort(coor);
+
+            foreach ((int x, int y) point in coor)
+                sw.WriteLine($"{point.x} {point.y}");
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
