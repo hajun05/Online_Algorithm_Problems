b9d074b906dbeab8cb41d0b2fbbc1efb02298653
hajun05 <hajun0325175@gmail.com>
2024-12-17 13:11:02 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 72 ms, Memory: 5556 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+
+namespace CsWorkspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(ReadLine());
+            WriteLine($"{n}\n{1}");
+        }
+    }
+}
