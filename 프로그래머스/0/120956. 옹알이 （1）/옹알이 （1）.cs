e722ca0ee74f4738550d5964cb3752488717d29e
Hajun Oh <hajun0325175@gmail.com>
2025-04-01 14:05:10 +0900

    [level 0] Title: 옹알이 (1), Time: 0.89 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution 
+{
+    public int solution(string[] babbling) 
+    {
+        int answer = 0;
+        string[] talkable = { "aya", "ye", "woo", "ma" };
+        for (int i = 0; i < babbling.Length; i++) 
+        {
+            string word = babbling[i];
+            foreach (string able in talkable)
+            {
+                word = word.Replace(able, " ");
+                if(string.IsNullOrWhiteSpace(word))
+                {
+                    answer++;
+                    break;
+                }
+            }
+            
+        }
+
+        return answer;
+    }
+}
