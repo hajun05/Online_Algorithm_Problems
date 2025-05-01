ee93877fe474bf36a665e184d8ce4c61fdfc6b86
Hajun Oh <hajun0325175@gmail.com>
2025-05-01 14:58:22 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.52 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cs
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cs
@@ -2,41 +2,36 @@ using System;
 
 public class Solution 
 {
-    // 달팽이 배열 알고리즘 문제
     public int[,] solution(int n) 
     {
-        // 닶을 저장할 2차원 배열
-        int[,] answer = new int[n,n];
-        // 배열에 저장할 값
+        int[,] answer = new int[n, n];
         int num = 1;
         
-        // 좌표 설정
-        int row = 0;
-        int col = 0;
+        // 경계값 설정
+        int startRow = 0, endRow = n - 1;
+        int startCol = 0, endCol = n - 1;
         
-        // 시작점 설정
-        answer[row, col] = num++;
-        
-        // 달팽이 배열 순회
-        // 배열을 순회할때 배열 범위를 넘어서지 않도록 주의
-        while(num <= n * n)
+        while (num <= n * n)
         {
-            while (col + 1 < n && answer[row, col + 1] == 0)
-            {
-                answer[row, ++col] = num++;
-            }
-            while (row + 1 < n && answer[row + 1, col] == 0)
-            {
-                answer[++row, col] = num++;
-            }
-            while (col - 1 >= 0 && answer[row, col - 1] == 0)
-            {
-                answer[row, --col] = num++;
-            }
-            while (row - 1 >= 0 && answer[row - 1, col] == 0)
-            {
-                answer[--row, col] = num++;
-            }
+            // 1. 오른쪽으로 이동 (상단 행)
+            for (int col = startCol; col <= endCol; col++)
+                answer[startRow, col] = num++;
+            startRow++;
+            
+            // 2. 아래로 이동 (우측 열)
+            for (int row = startRow; row <= endRow; row++)
+                answer[row, endCol] = num++;
+            endCol--;
+            
+            // 3. 왼쪽으로 이동 (하단 행)
+            for (int col = endCol; col >= startCol; col--)
+                answer[endRow, col] = num++;
+            endRow--;
+            
+            // 4. 위로 이동 (좌측 열)
+            for (int row = endRow; row >= startRow; row--)
+                answer[row, startCol] = num++;
+            startCol++;
         }
         
         return answer;
