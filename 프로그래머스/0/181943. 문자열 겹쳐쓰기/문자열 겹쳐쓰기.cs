7f7343a6b518ffc1887375e05cb35d1403f1ae26
Hajun Oh <hajun0325175@gmail.com>
2025-08-22 16:44:03 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.39 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Text;
 
 public class Solution 
 {
     public string solution(string my_string, string overwrite_string, int s) 
     {
-        int overwirteLength = overwrite_string.Length;
-        string answer = my_string.Remove(s, overwirteLength).Insert(s, overwrite_string);
+        StringBuilder answerBuilder = new StringBuilder();
+        
+        answerBuilder.Append(my_string.Substring(0, s));
+        answerBuilder.Append(overwrite_string);
+        answerBuilder.Append(my_string.Substring(s + overwrite_string.Length));
+        
+        string answer = answerBuilder.ToString();
         
         return answer;
     }
